22b08f41b711f9295aedf2042fc3584d3a9a9c4d
EC2 Default User <ec2-user@ip-172-31-87-12.ec2.internal>
2021-11-17 13:15:27 +0000

    住所カラム完了

--- db/migrate/20211117061723_create_orders.rb
+++ db/migrate/20211117061723_create_orders.rb
@@ -1,15 +1,15 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-      t.integer :end_user_id
-      t.string :postcode
-      t.string :address
-      t.string :name
-      t.integer :shipping_fee
-      t.integer :total_price
-      t.integer :peyment_method
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.integer :end_user_id#会員id
+      t.string :postcode#郵便番号
+      t.string :address#配送先の住所
+      t.string :name#氏名
+      t.integer :shipping_fee#配送料
+      t.integer :total_price#合計金額
+      t.integer :peyment_method#支払方法
+      t.datetime :created_at#登録日
+      t.datetime :updated_at#更新日
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211117061723) do
+ActiveRecord::Schema.define(version: 20211117130617) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer  "end_user_id"
+    t.string   "postcode"
+    t.string   "addresses"
+    t.string   "name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
