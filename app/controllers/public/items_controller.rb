e265c3a8511a36dbf597661ae7de45ad9a264de0
kn1025yu <sn22kzh@gmail.com>
2021-11-20 11:32:09 +0000

    [fix]コントローラのアクション定義

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,4 +1,9 @@
 class Public::ItemsController < ApplicationController
 
-end
+  def index
+  end
 
+  def show
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
     resources :order_details, only: [:update]
 
     #管理者用のルーティング
-    devise_for :admin,skip:[:registrations, :passwords], controllers: {
-    sessions:'admin/sessions'
+    devise_for :admins,skip:[:registrations, :passwords], controllers: {
+    sessions:'admins/sessions'
   }
   end
 #エンドユーザー側のルーティング設定
@@ -19,12 +19,11 @@ devise_for :end_users,skip: [:passwords,], controllers: {
   registrations: 'public/registrations'
 }
 
-
   namespace :admin do
     resources :items, except: [:destory]
     resources :orders, only: [:show, :update]
     resources :order_datails, only: [:update]
-    resources :cates
+    resources :categorys
   end
 
   namespace :public do
@@ -37,12 +36,11 @@ devise_for :end_users,skip: [:passwords,], controllers: {
     resources :cart_items, only: [:index, :create, :update, :destory] do
       delete 'destroy_all'
     end
-    resources :genres, only: [:index]
+    resources :categorys, only: [:index]
     resources :orders, only: [:index, :show, :new, :create] do
       post 'order/confirm' => 'orders#confirm'
       get 'complete'
     end
     resources :address, except: [:new, :show]
   end
-
 end
