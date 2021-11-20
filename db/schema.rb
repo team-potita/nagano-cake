35ffab52186f257ce6ddf8b2ad3393b35a849fc6
goshi54 <g.o.s.h.i.cb400@gmail.com>
2021-11-20 04:01:04 +0000

    Firstcommit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_070304) do
+ActiveRecord::Schema.define(version: 2021_11_19_062959) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "end_user_id"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2021_11_18_070304) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "end_user_id"
+    t.integer "item_id"
+    t.integer "quantity"
   end
 
   create_table "categories", force: :cascade do |t|
