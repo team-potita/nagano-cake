595d6be99e41edf2bd3999fcc96a9c89a1df33d5
EC2 Default User <ec2-user@ip-172-31-87-12.ec2.internal>
2021-11-20 03:40:45 +0000

    アクション定義

--- app/controllers/public/address_controller.rb
+++ app/controllers/public/address_controller.rb
@@ -1,2 +1,17 @@
 class Public::AddressController < ApplicationController
+  
+  def index
+  end
+  
+  def edit
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
 end
