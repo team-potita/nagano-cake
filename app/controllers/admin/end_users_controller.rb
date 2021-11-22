c70b9f4f9ec4df8fdc20589bede05332f7f8e235
EC2 Default User <ec2-user@ip-172-31-87-12.ec2.internal>
2021-11-22 09:39:54 +0000

    編集

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -31,3 +31,10 @@ class Admin::EndUsersController < ApplicationController
     end
 end
 
+def full_name
+  self.last_name + " " + self.first_name
+end
+
+def full_name_kana
+  self.last_name + " " + self.first_name
+end
