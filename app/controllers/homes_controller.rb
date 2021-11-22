a89dca99cea2ad5c4215262631a0bfd50e95f529
goshi54 <g.o.s.h.i.cb400@gmail.com>
2021-11-22 05:55:34 +0000

    item controllerの編集

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,10 @@
 class HomesController < ApplicationController
+  before_action :authenticate_customer!, except: [:top]
   def top
+    @category = Category.new
+    @items = Item.all
   end
-  
+
   def about
   end
 end
