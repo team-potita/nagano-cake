0e8365a2a7c5435b62fb66483b5ca0bc69089d4f
kn1025yu <sn22kzh@gmail.com>
2021-11-23 07:14:59 +0000

    [add]order編集

--- Gemfile
+++ Gemfile
@@ -52,3 +52,5 @@ gem 'devise'
 
 gem 'bootstrap', '~> 4.5'
 gem 'jquery-rails'
+
+gem "enum_help"
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
-  
+
   belongs_to :item
-  
+  enum payment_method: { credit_card: 0, transfer: 1 }
+
 end
