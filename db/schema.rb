619a4ff115390483356f94042f311f31b5b99a58
EC2 Default User <ec2-user@ip-172-31-87-12.ec2.internal>
2021-11-27 06:03:23 +0000

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_25_123516) do
+ActiveRecord::Schema.define(version: 2021_11_27_054324) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "end_user_id"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2021_11_25_123516) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "last_name"
@@ -62,7 +63,7 @@ ActiveRecord::Schema.define(version: 2021_11_25_123516) do
     t.string "postcode"
     t.string "tel"
     t.string "address"
-    t.boolean "is_valid"
+    t.boolean "is_valid", default: true
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
